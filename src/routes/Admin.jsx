1a1b2672eca505b01cb87852c23e8bccfe94320f
project2479 <jaywrsnp6@gmail.com>
2025-07-01 14:13:31 +0800

    new

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -43,7 +43,7 @@ function Admin() {
 				className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
 				aria-label="Sidebar">
 				<div className="h-full px-3 py-4 overflow-y-auto bg-blue-800">
-					<ul className="space-y-2 font-medium">
+					<ul className="space-y-2 font-medium pt-24">
 						<li>
 							<a
 								href="#"
